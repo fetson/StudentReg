7f95b9a4dc06affd7060a816974d3fad3d179e84
fetson <34402661+fetson@users.noreply.github.com>
2017-12-09 13:46:56 -0800

    Initial commit

---
+++ StudentReg/Models/EntityManager/UserManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudentReg.Models.DB;
+using StudentReg.Models.ViewModel;
+
+namespace StudentReg.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(UserSignUpView user) {
+
+            using (Student_RegDB db = new Student_RegDB()) {
+
+                SYSUser SU = new SYSUser();
+                SU.LoginName = user.LoginName;
+                SU.PasswordEncryptedText = user.Password;
+                SU.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1; ;
+                SU.RowCreatedDateTime = DateTime.Now;
+                SU.RowMOdifiedDateTime = DateTime.Now;
+
+                db.SYSUsers.Add(SU);
+                db.SaveChanges();
+
+                SYSUserProfile SUP = new SYSUserProfile();
+                SUP.SYSUserID = SU.SYSUserID;
+                SUP.FirstName = user.FirstName;
+                SUP.LastName = user.LastName;
+                SUP.Gender = user.Gender;
+                SUP.Phone = user.Phone;
+                SUP.Address = user.Address;
+                SUP.City = user.City;
+                SUP.Country = user.Country;
+                SUP.Program = user.Program;
+                SUP.Department = user.Department;
+                SUP.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowCreatedDateTime = DateTime.Now;
+                SUP.RowModifiedDateTime = DateTime.Now;
+
+                db.SYSUserProfiles.Add(SUP);
+                db.SaveChanges();
+
+
+                if (user.LOOKUPRoleID > 0) {
+                    SYSUserRole SUR = new SYSUserRole();
+                    SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                    SUR.SYSUserID = user.SYSUserID;
+                    SUR.IsActive = true;
+                    SUR.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowCreatedDateTime = DateTime.Now;
+                    SUR.RowModifiedDateTime = DateTime.Now;
+
+                    db.SYSUserRoles.Add(SUR);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public bool IsLoginNameExist(string loginName) {
+            using (Student_RegDB db = new Student_RegDB()) {
+                return db.SYSUsers.Where(o => o.LoginName.Equals(loginName)).Any();
+            }
+        }
+    }
+}
---
+++ StudentReg/Models/ViewModel/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace StudentReg.Models.ViewModel
+{
+
+    public class UserSignUpView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Program { get; set; }
+        public string Department { get; set; }
+       
+    }
+}
